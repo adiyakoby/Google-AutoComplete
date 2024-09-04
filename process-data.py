a7c5e18628d8a220c98a6c30edd8809acc04cb6d
Adi <Adi.Yakoby@gmail.com>
2024-09-04 10:13:43 +0300

    improved running time by using range and refrencing the list of sub strings

--- process-data.py
+++ process-data.py
@@ -8,21 +8,22 @@ class ProcessData:
     def __init__(self):
         self.__data = defaultdict(list)
 
-    def get_all_substrings(self, s: str) -> List[str]:
+    def get_all_substrings(self, s: str, sub_strings: List[str]):
         sub_strings = [s[i:j] for i, j in itertools.combinations(range(len(s) + 1), 2)]
-        return sub_strings
+
 
     def remove_punctuation(self, line):
         return " ".join(re.sub(r'[^\w\s]', '', line.lower()).split())
 
     def process(self, lines: List, filename: str):
-        for i, line in enumerate(lines, 1):
-            clean_line = self.remove_punctuation(line.strip())
+        sub_strings = []
+        for i in range(len(lines)):
+            clean_line = self.remove_punctuation(lines[i].strip())
 
             if clean_line:
-                substrings = self.get_all_substrings(clean_line)
-                for substring in substrings:
-                    self.__data[substring].append({"content": line, "index": i, "filename": filename})
+                self.get_all_substrings(clean_line, sub_strings)
+                for substring in sub_strings:
+                    self.__data[substring].append({"content": lines[i], "index": i+1, "filename": filename})
 
     def get_data(self):
         return self.__data
